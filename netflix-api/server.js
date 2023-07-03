9b881d05a308b2374a7b26cf17fb2e749348fdb9
Davi Luiz <davilucena222@gmail.com>
2023-07-03 23:06:53 +0000

    tryingg to connect to mongoDB

--- netflix-api/server.js
+++ netflix-api/server.js
@@ -7,12 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/netflix", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log("DB connected!");
-});
+mongoose.connect("mongodb://localhost:27017/netflix").then(result => console.log("DB Connected!")).catch((error) => console.error(error)); 
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000!");
